ec55f8b5988b4e7e445b993eb45c30d50ba492bb
saverio <javier.castiarena@gmail.com>
2013-12-01 00:31:58 -0300

    Commented out line

--- library/Zend/Feed/Reader/Reader.php
+++ library/Zend/Feed/Reader/Reader.php
@@ -238,7 +238,7 @@ class Reader
         $headers = new ZendHttp\Headers();
         $client->setHeaders($headers);
 
-        // Set a user-agent to avoid 403 error status 
+        // Set a user-agent to avoid 403 error status
         $headers->addHeaderLine('User-Agent', 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1467.0 Safari/537.36');
 
         $client->setUri($uri);
